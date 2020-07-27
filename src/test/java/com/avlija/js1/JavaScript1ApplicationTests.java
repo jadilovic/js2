1bf3e4f89785a1da2834b76217017d9228f94796
jadilovic <adilovic79@yahoo.com>
2020-07-27 21:38:36 +0200

    first commit

---
+++ src/test/java/com/avlija/js1/JavaScript1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avlija.js1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaScript1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
