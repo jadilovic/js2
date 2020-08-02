ee19606a58194b9d54317a0ee19ebcd0f1af9931
jadilovic <adilovic79@yahoo.com>
2020-08-02 09:41:24 +0200

    new java script

--- src/main/resources/static/app2.js
+++ src/main/resources/static/app2.js
@@ -47,3 +47,103 @@ const car = {
 		  color: 'blue'
 		}
 console.log(car.brand.name);
+
+car.brand.name = "VW";
+
+console.log(car.brand.name);
+
+console.log(car);
+
+const car2 = {
+		brand: "Opel",
+		model: "Corsa",
+		start: function(){
+			console.log('Started ${this.brand} ${this.model}');
+		}
+	}
+
+car2.start();
+
+const car3 = {
+		brand: "Toyota",
+		model: "Corolla",
+		goTo: function(destination){
+			console.log('Going to ${destination}');
+		}
+	}
+
+car3.goTo("Rome");
+
+console.log(car3.brand);
+
+class Person{
+	hello(){
+		return "Hello I am Florian";
+	}
+}
+
+const person = new Person();
+person.hello();
+
+console.log(person.hello());
+
+
+class Person1{
+	
+	constructor(name){
+		this.name = name;
+	}
+	
+	hello(){
+		return "Hello I am " + this.name + ".";
+	}
+	
+}
+
+const person1 = new Person1("Jasmin");
+person1.hello();
+
+console.log(person1.hello());
+
+
+class Person2 {
+	static genericHello(){
+		return "Hello Person";
+	}
+}
+const person2 = new Person2();
+
+console.log(Person2.genericHello());
+
+class Programer extends Person {
+	hellop(){
+		return super.hello() + " and Programer";
+	}
+}
+
+//console.log(Programer.hello());
+
+const prog = new Programer();
+
+console.log(prog.hellop());
+
+console.log("before");
+
+setTimeout(() => {
+	// runs after 2 seconds
+	console.log("inside the function");
+}, 2000)
+
+console.log("after");
+
+const doSomething = new Promise(
+		  (resolve, reject) => {
+		    //some code
+		    const success = "100";
+		    if (success) {
+		      resolve(console.log('ok'))
+		    } else {
+		      reject(console.log('this error occurred'))
+		    }
+		  }
+		)
